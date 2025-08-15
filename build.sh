a240a231fd6747a48c83b6fd459e8cb0bcb410e9
Vedang Deshmukh <vedangdeshmukh777@gmail.com>
2025-08-15 14:48:50 +0530

    Fix Pillow compatibility and build script

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip and install build tools first
+pip install --upgrade pip setuptools wheel
+
+# Install requirements
 pip install -r requirements.txt
 
+# Run Django management commands
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_superuser_auto
